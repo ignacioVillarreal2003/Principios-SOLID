0ba20a6359c3d9f753a8a7141515655f2d6abb76
ignacioVillarreal2003 <ignaciovillarreal20031231@gmail.com>
2023-12-31 16:00:48 -0300

    update

---
+++ principio-AbiertoCerrado/src/Library/Elemplo1.cs
@@ -0,0 +1,39 @@
+﻿namespace Library;
+
+public class Bebidas
+{
+    public string Nombre { get; set; }
+    public string Tipo { get; set; }
+    public decimal Precio { get; set; }
+   
+}
+
+public class Factura
+{
+    public decimal ObtenerTotal(IEnumerable<Bebidas> listaBebidas)
+    {
+        decimal total = 0;
+        foreach (var bebida in listaBebidas)
+        {
+            if (bebida.Tipo == "Agua")
+            {
+                total += bebida.Precio;
+            }
+            else if (bebida.Tipo == "Cola")
+            {
+                total += bebida.Precio * 0.33m;
+            }
+            else if (bebida.Tipo == "Alcohol")
+            {
+                total += bebida.Precio * 0.71m;
+            }
+        }
+        return total;
+    }
+}
+
+
+
+
+    
+
---
+++ principio-AbiertoCerrado/src/Library/Elemplo2.cs
@@ -0,0 +1,56 @@
+﻿namespace Library;
+
+public interface IBebida
+{
+    public string Nombre { get; set; }
+    public decimal Precio { get; set; }
+    public decimal Impuesto { get; set; }
+    public decimal ObtenerPrecio();
+}
+
+public class Agua : IBebida
+{
+    public string Nombre { get; set; }
+    public decimal Precio { get; set; }
+    public decimal Impuesto { get; set; }
+    public decimal ObtenerPrecio()
+    {
+        return Precio * Impuesto;
+    }
+}
+
+public class Alcohol : IBebida
+{
+    public string Nombre { get; set; }
+    public decimal Precio { get; set; }
+    public decimal Impuesto { get; set; }
+    public decimal Promo { get; set; }
+    public decimal ObtenerPrecio()
+    {
+        return (Precio * Impuesto) - Promo;
+    }
+}
+
+public class Cola : IBebida
+{
+    public string Nombre { get; set; }
+    public decimal Precio { get; set; }
+    public decimal Impuesto { get; set; }
+    public decimal ObtenerPrecio()
+    {
+        return Precio * Impuesto;
+    }
+}
+
+public class Factura2
+{
+    public decimal ObtenerTotal(IEnumerable<IBebida> listaBebidas)
+    {
+        decimal total = 0;
+        foreach (var bebida in listaBebidas)
+        {
+            total += bebida.ObtenerPrecio();
+        }
+        return total;
+    }
+}
